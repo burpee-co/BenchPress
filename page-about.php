37888cfde54cbd3ddb31b9a6c8aeb1c9746b6436
Ryle Garcia <ryle.garcia@gmail.com>
2015-05-28 15:23:15 -0400

    added typography partial lol

--- page-about.php
+++ page-about.php
@@ -7,18 +7,25 @@
   get_header();  ?>
 
   <div class="main">
+    <div class="half-hero">
+       <div class="overlay">
+           <h1><?php echo get_bloginfo ('title') ?></h1>
+           <h2><?php echo get_bloginfo ('description'); ?></h2>
+       </div>
+      <img src="images/kettlebell3.jpg" alt="">
+    </div>
     <div class="container aboutContainer">
   
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
       
+<!--         <div class="aboutHeader">
+          <?php wp_get_attachment_image(); ?>
+        </div> -->
+
         <div class="aboutContent">
           <h2><?php the_title(); ?></h2>
           <?php the_content(); ?>
-        </div>
-
-        <div class="aboutCert">
-          <h3>Certifications</h3>
         </div>
 
       <?php endwhile; // end the loop?>
