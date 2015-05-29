39155c576810def963f76e1c8577e72edbdf9b5a
Ryle Garcia <ryle.garcia@gmail.com>
2015-05-29 15:16:20 -0400

    updated contact page

--- page-contact.php
+++ page-contact.php
@@ -7,12 +7,19 @@
 get_header();  ?>
 
 <div class="main">
+	<div class="half-hero halfHeroAbout">
+	   <div class="overlay">
+	      <?php // Start the loop ?>
+	      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	       <?php endwhile; // end the loop?>
+	       <h2><?php the_title(); ?></h2>
+	   </div>
+	</div>
   <div class="container">
 
     <?php // Start the loop ?>
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-      <h2><?php the_title(); ?></h2>
       <?php the_content(); ?>
 
     <?php endwhile; // end the loop?>
