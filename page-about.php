884c9a233311bd99a3e780182e45699548bb567a
tynsoltys <c.n.soltys@gmail.com>
2015-05-29 16:48:28 -0400

    added half-hero to remaining templates

--- page-about.php
+++ page-about.php
@@ -6,8 +6,13 @@
 
   get_header();  ?>
 
+  <?php global $post; ?>
+  <?php
+  $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
+  ?>
+
   <div class="main">
-    <div class="half-hero halfHeroAbout">
+    <div class="half-hero halfHeroAbout" style="background: url(<?php echo $src[0]; ?> ) !important; background-size: cover !important; background-attachment: fixed !important;">
        <div class="overlay">
            <h2><?php the_title(); ?></h2>
           <?php // Start the loop ?>
