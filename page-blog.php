bdf18cf5d61e0ec7ffcf2d3f93f08343592fa583
tynsoltys <c.n.soltys@gmail.com>
2015-05-31 16:24:54 -0400

    sidebar updates

--- page-blog.php
+++ page-blog.php
@@ -1,5 +1,10 @@
-<?php get_header();  ?>
+<?php
 
+/*
+	Template Name: Blog Page
+*/
+
+get_header();  ?>
 <?php global $post; ?>
 <?php
 $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
@@ -9,20 +14,14 @@ $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 560
 
   <div class="half-hero halfHeroAbout" style="background: url(<?php echo $src[0]; ?> ) !important; background-size: cover !important; background-attachment: fixed !important;">
      <div class="overlay">
-         <h2><?php wp_title(); ?></h2>
+         <h2><?php BLOG ?></h2>
      </div>
   </div>
   <div class="container">
 
     <div class="content">
-      <?php // Start the loop ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-
-      <?php endwhile; // end the loop?>
-    </div> <!-- /,content -->
+    		<?php get_template_part( 'loop', 'index' );	?>
+    </div> <!--/.content -->
 
     <?php get_sidebar(); ?>
 
