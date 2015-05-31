cd074929aad5f4d7d5d272811476e1aeb4e62387
Ryle Garcia <ryle.garcia@gmail.com>
2015-05-31 14:35:00 -0400

    just letting you know I'm in this file, editing some things with the layout and styles

--- author.php
+++ author.php
@@ -4,6 +4,8 @@
 $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
 ?>
 
+<!-- Tyn, just letting you know I'm editing some of the stuff here -->
+
 <div class="main">
 
   <div class="half-hero halfHeroAbout" style="background: url(<?php echo $src[0]; ?> ) !important; background-size: cover !important; background-attachment: fixed !important;">
